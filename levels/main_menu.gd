335953eb02e3589655f2e6da9327576f0b84ef3c
Dario Trinchero <dario.trinchero@pm.me>
2023-11-19 07:34:39 +0200

    🐐 Improve main menu.
    Add shortcut (Enter) to start game.
    Fix bug with goats on Misty Mountains becoming the same player.

--- levels/main_menu.gd
+++ levels/main_menu.gd
@@ -1,13 +1,13 @@
+class_name MainMenu
 extends Node
 
+@onready var level_select: OptionButton = %level_select as OptionButton
 
-func _on_in_the_mines_btn_button_up() -> void:
-	get_tree().change_scene_to_file("res://levels/in_the_mines.tscn")
+const levels = [
+	"res://levels/misty_mountains.tscn",
+	"res://levels/in_the_mines.tscn",
+	"res://levels/anthills.tscn"
+]
 
-
-func _on_misty_mountains_btn_button_up() -> void:
-	get_tree().change_scene_to_file("res://levels/misty_mountains.tscn")
-
-
-func _on_anthills_btn_button_up() -> void:
-	get_tree().change_scene_to_file("res://levels/anthills.tscn")
+func _on_button_pressed() -> void:
+	get_tree().change_scene_to_file(levels[level_select.selected])
