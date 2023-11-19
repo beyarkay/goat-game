a33c7c87b5c680800ae3cbd9e7d303af9ee90f12
Dario Trinchero <dario.trinchero@pm.me>
2023-11-19 17:16:35 +0200

    🐐 Improve cablecar visuals.

--- scenes/cable_car.gd
+++ scenes/cable_car.gd
@@ -2,6 +2,8 @@ class_name CableCar
 extends Path2D
 
 @export_range(0.5, 1.5) var speed: float = 1.0
+@export_range(0, 1) var start_fraction: float = 0
+@export_range(0, 1) var end_fraction: float = 1
 
 @onready var line: Line2D = $line as Line2D
 @onready var mover: AnimationPlayer = $PathFollow2D/mover
@@ -11,6 +13,10 @@ extends Path2D
 func _ready() -> void:
 	line.points = curve.get_baked_points()
 	mover.speed_scale = speed
+	var move_animation := mover.get_animation("move_platform")
+	move_animation.track_set_key_value(0, 0, start_fraction)
+	move_animation.track_set_key_value(0, 1, end_fraction)
+	move_animation.track_set_key_value(0, 2, start_fraction)
 
 func _on_pitch_shift_timer_timeout() -> void:
 	rail_sound.pitch_scale = randf_range(0.8, 1.2)
